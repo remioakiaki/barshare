c5293706c5cccc2d668c9f72609ba12d18ddae6e
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-08-04 08:51:23 +0900

    Session関連追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
