f2ae4717d3b532902b363ec3a729f2ec9f0b3eda
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-08-04 07:23:50 +0900

    Userモデルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # attr_accessor :remember_token
+  # has_many :microposts, dependent: :destroy
+  # has_many :active_relationships, class_name: 'Relationship',
+  #                                 foreign_key: 'follower_id',
+  #                                 dependent: :destroy
+  # has_many :passive_relationships, class_name: 'Relationship',
+  #                                  foreign_key: 'followed_id',
+  #                                  dependent: :destroy
+  # has_many :following, through: :active_relationships, source: :followed
+  # has_many :followers, through: :passive_relationships, source: :follower
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                     format: { with: VALID_EMAIL_REGEX },
+                     uniqueness: { case_sensitive: false }
+   has_secure_password
+   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  # validates :introduce, length: { maximum: 100 }
+  # mount_uploader :picture, PictureUploader
+  # validate :picture_size
+end
