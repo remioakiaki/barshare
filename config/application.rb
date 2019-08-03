f3e2264ea7c0857c6ae1cd097457dd9e6b69564b
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-08-03 16:38:07 +0900

    Rspecの設定追加

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,17 @@ module Barshare
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      # 色々な記述があるので、一番下に追記する
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
   end
 end
