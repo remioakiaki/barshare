61f660291f4e2284be00cbc716d557ec73decf30
AKINORI NAKAHARA <railiminal.garnet@gmail.com>
2019-08-04 10:02:47 +0900

    日本語化対応とDB変更履歴

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_222146) do
+ActiveRecord::Schema.define(version: 2019_08_04_003249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_08_03_222146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "picture"
+    t.text "introduce"
+    t.boolean "admin"
+    t.text "remember_digest"
   end
 
 end
